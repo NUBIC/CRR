4f54c59879a972433dc2bfccf41feae476438791
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-11-06 14:21:43 -0600

    Remove child flag from account_participant.

--- app/models/account_participant.rb
+++ app/models/account_participant.rb
@@ -8,7 +8,6 @@
 #  proxy          :boolean          default(FALSE), not null
 #  created_at     :datetime
 #  updated_at     :datetime
-#  child          :boolean          default(FALSE), not null
 #
 
 class AccountParticipant < ActiveRecord::Base

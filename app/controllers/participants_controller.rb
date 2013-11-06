eebdec1419f22544101396152342df53cef76822
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-11-06 15:51:18 -0600

    Set the child flag for participant.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -28,8 +28,11 @@ class ParticipantsController < PublicController
     @participant = Participant.create!
     account_participant = AccountParticipant.new(:participant => @participant, :account => @account)
     account_participant.proxy = true if params[:proxy] == "true"
-    account_participant.child = true if params[:child] == "true"
     account_participant.save
+    if params[:child] == "true"
+      @participant.child = true
+      @participant.save
+    end
     redirect_to enroll_participant_path(@participant)
   end
 

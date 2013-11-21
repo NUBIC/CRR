2af6a0fcb0755edc6e9033a69c1e1eda58861bbb
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-11-21 11:34:25 -0600

    Fix the typo in the survey mailer.

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -2,7 +2,7 @@ class SurveyMailer < ActionMailer::Base
   default :from => "noreply@commresearchregistry.northwestern.edu"
 
   def new_survey_alert(response_set)
-    @recipients =  [response_set.particpant.email ,response_set.participant.account.email].flatten.uniq
+    @recipients =  [response_set.participant.email ,response_set.participant.account.email].flatten.uniq
     mail(:to => @recipients, :subject => "[registry] survey alert")
   end
 

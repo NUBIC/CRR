383e1af6613dacfc7caa93f45eb89e29e040878c
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-12-02 10:50:35 -0600

    if current_user then redirect to the dashboard.

--- app/controllers/account_sessions_controller.rb
+++ app/controllers/account_sessions_controller.rb
@@ -2,16 +2,20 @@ class AccountSessionsController < PublicController
   # GET /account_sessions/new
   def new
     @account_session = AccountSession.new
-     
+
     respond_to do |format|
-      format.html # new.html.erb
+      if current_user
+        format.html { redirect_to dashboard_path }
+      else
+        format.html
+      end
     end
   end
-   
+
   # POST /account_sessions
   def create
     @account_session = AccountSession.new(params[:account_session])
-     
+
     respond_to do |format|
       if @account_session.save
         format.html { redirect_to dashboard_path
@@ -21,7 +25,7 @@ class AccountSessionsController < PublicController
       end
     end
   end
-   
+
   # DELETE /account_sessions/1
   def destroy
     @account_session = AccountSession.find

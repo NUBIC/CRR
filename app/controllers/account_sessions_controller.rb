8280ab8443f075e2ff3c9334f221fde6a65b2eea
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-12-05 13:16:22 -0600

    Display error message if login has any error.

--- app/controllers/account_sessions_controller.rb
+++ app/controllers/account_sessions_controller.rb
@@ -15,13 +15,11 @@ class AccountSessionsController < PublicController
   # POST /account_sessions
   def create
     @account_session = AccountSession.new(params[:account_session])
-
-    respond_to do |format|
-      if @account_session.save
-        format.html { redirect_to dashboard_path }
-      else
-        format.html { render :action => "new" }
-      end
+    if @account_session.save
+      redirect_to dashboard_path
+    else
+      flash[:error] = @account_session.errors.full_messages.to_sentence
+      redirect_to public_login_path(:anchor => "login_tab")
     end
   end
 

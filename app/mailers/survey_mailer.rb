ac3702e632e06634cd22e655c2e1a4f2584c545a
kumbafu <mutero_d@yahoo.com>
2013-12-14 14:56:22 -0600

    added email text from kathy

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -3,7 +3,7 @@ class SurveyMailer < ActionMailer::Base
 
   def new_survey_alert(response_set)
     @recipients =  [response_set.participant.email ,response_set.participant.account.email].flatten.uniq
-    mail(:to => @recipients, :subject => "[registry] survey alert")
+    mail(:to => @recipients, :subject => "Request for more information in your Registry profile")
   end
 
 end

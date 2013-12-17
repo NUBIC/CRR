f3b338f18f75e0c661345a504ab9966f0cc829ac
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-12-16 21:53:58 -0600

    Set no_cache for browser back button.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :flash_headers
+  before_filter :set_no_cache
 
   def flash_headers
     # This will discontinue execution if Rails detects that the request is not
@@ -13,6 +14,12 @@ class ApplicationController < ActionController::Base
     response.headers['x-flash-errors'] = flash[:error]  unless flash[:error].blank?
     # Stops the flash appearing when you next refresh the page
     flash.discard
+  end
+
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
   end
 
 end

fae6fad8abedca52504dd76ff02b59a049f87d18
Jalpa Patel <jalpa-patel@northwestern.edu>
2014-07-08 14:24:23 -0500

    More changes for dashboard. #11056.

--- app/assets/javascripts/jquery.main.js
+++ app/assets/javascripts/jquery.main.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
   $('#dashboard_list').livequery(function(){$(this).dataTable( {
   "bScrollCollapse": true,
   "sPaginationType": "bootstrap",
-  "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+  "sDom": "<'row-fluid'<'span6 pending-info'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
   "sWrapper": "dataTables_wrapper form-inline",
   "aaSorting": [],
   "bFilter": true,

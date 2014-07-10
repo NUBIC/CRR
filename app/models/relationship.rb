fedef189a711c17564faed0928f17e6743676f56
Jalpa Patel <jalpa-patel@northwestern.edu>
2014-07-10 16:20:15 -0500

    Change Sibling label. #11159.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -12,9 +12,9 @@
 #
 
 class Relationship < ActiveRecord::Base
-  CATEGORIES = ['Sibling or Half-sibling','Child','Parent', 'Guardian', 'Spouse', 'Not Applicable']
+  CATEGORIES = ['Sibling or Half sibling','Child','Parent', 'Guardian', 'Spouse', 'Not Applicable']
 
-  CATEGORIES_DESTINATION_TRANSLATION = {'Sibling or Half-sibling'=>'Sibling or Half-sibling','Spouse'=>'Spouse','Parent'=>'Child','Child'=>'Parent', 'Guardian'=>'Ward', 'Not Applicable' => 'Not Applicable'}
+  CATEGORIES_DESTINATION_TRANSLATION = {'Sibling or Half sibling'=>'Sibling or Half sibling','Spouse'=>'Spouse','Parent'=>'Child','Child'=>'Parent', 'Guardian'=>'Ward', 'Not Applicable' => 'Not Applicable'}
 
   belongs_to :origin, :class_name=>"Participant",:foreign_key=>"origin_id"
   belongs_to :destination, :class_name=>"Participant",:foreign_key=>"destination_id"

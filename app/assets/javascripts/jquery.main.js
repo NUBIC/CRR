9fe9a502c8093ba0726e9b01c3b1fd41948e19bc
Jalpa Patel <jalpa-patel@northwestern.edu>
2014-07-21 13:18:03 -0500

    Added consent tables. #11066.

--- app/assets/javascripts/jquery.main.js
+++ app/assets/javascripts/jquery.main.js
@@ -80,6 +80,23 @@ $(document).ready(function() {
       "sSearch": "Filter: "
         }});});
 
+  $('#consent_list').livequery(function(){
+    $(this).dataTable( {
+      "fnInfoCallback ": function() {showHideMore();},
+      "bScrollCollapse": true,
+      "sPaginationType": "bootstrap",
+      "sDom": "<'row-fluid'<'span6 consent-header'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+      "sWrapper": "dataTables_wrapper form-inline",
+      "aaSorting": [],
+      "bFilter": true,
+      "iDisplayLength": 30,
+      "bLengthChange": false,
+      "oLanguage": {
+          "sSearch": "Filter: ",
+      }
+    });
+  });
+
   $('#dashboard_list').livequery(function(){
     showHideMore();
     $(this).dataTable( {
@@ -224,6 +241,10 @@ $(document).ready(function() {
 
   $(".participant-header").livequery(function(){
     $(this).append($("<h4>" + $('#additonal-data').data("header") + "<a class='btn btn-primary nu-primary' style='margin-left: 15px' href=" + $('#additonal-data').data("url")+ ">Add a participant</a>" + "</h4>"));
+  });
+
+  $(".consent-header").livequery(function(){
+    $(this).append($("<h4>" + $('#additonal-data').data("header") + "<a class='btn btn-primary nu-primary' style='margin-left: 15px' href=" + $('#additonal-data').data("url")+ ">Add a consent</a>" + "</h4>"));
   });
 
   $('.consent-agree').attr('disabled', 'disabled');

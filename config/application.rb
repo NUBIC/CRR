15d7adcb7028fae89ce03ce3f50930b494b7829b
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2015-04-06 08:44:37 -0500

    Updated action mailer settings to use new server addresses.

--- config/application.rb
+++ config/application.rb
@@ -17,19 +17,20 @@ module AudiologyRegistry
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'config.yml')
+      APP_CONFIG = YAML.load(File.open(env_file))
+    end
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_mailer.smtp_settings = { :address => "smtprelay.northwestern.edu", :port => 25, :domain => "northwestern.edu" }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { :host => "http#{'s' unless Rails.env == 'development'}://#{APP_CONFIG[Rails.env]['server_name']}" }
 
     Aker.configure do
       ui_mode :cas
-    end
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'config.yml')
-      APP_CONFIG = YAML.load(File.open(env_file))
     end
   end
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,11 +69,6 @@ AudiologyRegistry::Application.configure do
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
-  config.action_mailer.delivery_method = :smtp
-  # TODO - Add configuration option so no need to change everytime we deploy two staging server
-  # config.action_mailer.default_url_options = { :host => "https://clinical-rails-staging2.nubic.northwestern.edu/crr" } # NUBIC Staging server
-  config.action_mailer.default_url_options = { :host => "https://crr-dev.soc.northwestern.edu" } # CRR Dev server
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
   config.middleware.use ExceptionNotification::Rack,

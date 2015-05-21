5212e7f03ffd236d3e13787df228b39274c948b0
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2015-05-21 14:15:39 -0500

    Fixed deploy script to load config directly.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application,  Rails.configuration.custom.app_config['application']
-set :repo_url,     Rails.configuration.custom.app_config['repository']
+APP_CONFIG = YAML.load(File.open('config/config.yml'))
+
+set :application,  APP_CONFIG['application']
+set :repo_url,     APP_CONFIG['repository']
 
 set :rvm_type, :system
 set :rvm_ruby_version, '2.1.2'
@@ -66,9 +68,9 @@ NameVirtualHost *:80
 NameVirtualHost *:443
 
 <VirtualHost *:80>
-  ServerName #{ Rails.configuration.custom.app_config[ fetch(:stage).to_s ]['server_name'] }
-  ServerAlias #{ Rails.configuration.custom.app_config[ fetch(:stage).to_s ]['server_alias'] }
-  Redirect permanent / https://#{ Rails.configuration.custom.app_config[ fetch(:stage).to_s ]['server_name'] }/
+  ServerName #{ APP_CONFIG[ fetch(:stage).to_s ]['server_name'] }
+  ServerAlias #{ APP_CONFIG[ fetch(:stage).to_s ]['server_alias'] }
+  Redirect permanent / https://#{ APP_CONFIG[ fetch(:stage).to_s ]['server_name'] }/
 </VirtualHost>
 
 <VirtualHost *:443>
@@ -76,7 +78,7 @@ NameVirtualHost *:443
   PassengerAppEnv #{ fetch(:stage) }
   PassengerRuby /usr/local/rvm/wrappers/ruby-#{ fetch(:rvm_ruby_version) }/ruby
 
-  ServerName #{ Rails.configuration.custom.app_config[ fetch(:stage).to_s ]['server_name'] }
+  ServerName #{ APP_CONFIG[ fetch(:stage).to_s ]['server_name'] }
 
   SSLEngine On
   SSLCertificateFile /etc/pki/tls/certs/crr-prod.cer

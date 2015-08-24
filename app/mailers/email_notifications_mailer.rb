8a5f49486b05d9e8c78a71f260a04c8f64fd990e
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2015-08-24 09:14:36 -0500

    HTML-formatted emails, take 2 CRR-887

--- app/mailers/email_notifications_mailer.rb
+++ app/mailers/email_notifications_mailer.rb
@@ -1,6 +1,9 @@
 class EmailNotificationsMailer < ActionMailer::Base
   def generic_email(email, content, email_subject)
     @content = content
-    mail(from: 'commresearchregistry@northwestern.edu', to: email, subject: email_subject)
+    @email_subject = email_subject
+    mail(from: 'commresearchregistry@northwestern.edu', to: email, subject: email_subject) do |format|
+      format.html { render layout: 'email'}
+    end
   end
 end

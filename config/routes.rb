cf7eb5c233708241de75f66bcdac54b9dc38d2a5
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2016-01-26 10:40:56 -0600

    Revert "Revert "Allow to import survey data""
    This reverts commit 2a28b3c0dd8ae3cb8790a618fffef08006cf38cf.

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,11 @@ AudiologyRegistry::Application.routes.draw do
         get   :preview
       end
     end
-    resources :response_sets
+    resources :response_sets do
+      member do
+        patch :load_from_file
+      end
+    end
     resources :study_involvements do
       collection do
         get :study

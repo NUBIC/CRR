9557a4ce9349c74792b2ebfb153a739cdccb41e9
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2016-01-26 21:00:01 -0600

    Fix migration to ignore active status of email notification.

--- db/migrate/20160113160633_update_welcome_email_notification_type.rb
+++ db/migrate/20160113160633_update_welcome_email_notification_type.rb
@@ -1,6 +1,6 @@
 class UpdateWelcomeEmailNotificationType < ActiveRecord::Migration
   def change
-    welcome_email = EmailNotification.active.find_by(email_type: 'Welcome')
+    welcome_email = EmailNotification.find_by(email_type: 'Welcome')
     welcome_email.email_type = EmailNotification::WELCOME_PARTICIPANT
     welcome_email.save!
   end

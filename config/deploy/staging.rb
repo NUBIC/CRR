e438b1b44c42de8fd333ca725416828cc4ea0a57
Yulia Bushmanova <y-bushmanova@northwestern.edu>
2018-06-08 10:30:35 -0500

    Fixed deploy config reference to deployer user. CRR-999

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ DEPLOY_CONFIG = YAML.load(File.open('config/deploy_config.yml'))
 
 set :stage, :staging
 set :app_host,    DEPLOY_CONFIG[fetch(:stage).to_s]['app_host']
-set :app_server,  "#{DEPLOY_CONFIG[fetch(:stage).to_s]['deployer']}@#{fetch(:app_host)}"
+set :app_server,  "#{DEPLOY_CONFIG['deployer']}@#{fetch(:app_host)}"
 
 role :web, fetch(:app_server)
 role :db, fetch(:app_server)
